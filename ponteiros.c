d799bfe8076fc62d94e37814ed5bb9b214702c25
guilherme <guilhermeserbai64@gmail.com>
2024-03-22 12:36:41 -0300

    atualizações

--- ponteiros.c
+++ ponteiros.c
@@ -1,11 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-    int valor = 30;
-    int *pointer = NULL;
-    pointer = &valor;
-
-    printf("%d", *pointer);
-    return 0;
+// exercicio 01
+int main01(){
+int i=30;
+int v[]={1,2,3,4};
+int * p= v; 
+int * w=&v[2];
+int * x=&i; 
 }
+
+//exercicio 02
+int main02(){
+int a=10;
+int b=20;
+int c=30;
+int d=40;
+int * v[]={&a,&b,&c,&d};
+int **p=v;
+for (int i = 0; i < 4; i++)
+{ 
+    printf("%d\n",**p);
+    p++;
+  
+    
+}
+}
+
+//exercicio 03
+int main03(){
+int v1[]={1,2,3};
+int v2[]={4,5,6};
+int *m[]={v1,v2};
+int * p1=v1;
+int * p2=v2;
+int ** pm=m;
+for (int i = 0; i < 2; i++)
+{
+    for (int e = 0; e < 3; e++)
+    {
+        printf("%d/n",m[i][e]);
+    }
+    
+}}
+
---
+++ ponteiros_e_tipos_agregados.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <cstring> // Para strcpy
+using namespace std;
+typedef struct produto {
+    int codigo;
+    char descricao[12];
+    float preco;
+} Produto;
+
+int main1() {
+    Produto prod = {1, "teclado", 98.50};
+    Produto *p = &prod;
+    int *pi = &prod.codigo;
+    char *pc = prod.descricao;
+    float *pf = &prod.preco;
+
+    prod.codigo = 2;
+    strcpy(prod.descricao, "mouse"); 
+    prod.preco = 100.00;
+    prod={2,"mouse",100.00};
+
+    cout << "Codigo: " << prod.codigo << endl;
+    cout << "Descricao: " << prod.descricao << endl;
+    cout << "Preco: " << prod.preco << endl;
+
+    return 0;
+}
+
+typedef struct produto{
+int codigo;
+char descricao[12]; 
+float preco;
+} Produto;
+
+int main2(){
+Produto prodA = {1, "Teclado", 98.50};
+Produto prodB = {2, "Mouse", 65.00};
+Produto prodC = {3, "Webcam", 130.00};
+Produto prodG = {2004,"headphone",340.00};
+Produto* vet[4];
+vet[0] = &prodA;
+vet[1] = &prodB;
+vet[2] = &prodB;
+vet[3] = &prodC;
+vet[3]->codigo=100;
+strcpy(vet[1]->descricao,"mouse sem fio");
+vet[3]=&prodG;
+}
